b8df1749cd93d60fbb1a6aeb25f5b0c6a5c079ed
Vipul Sharma <vipul.sharma@timesinternet.in>
2020-09-23 14:25:44 +0530

    viewmodel in progress

---
+++ app/src/main/java/entity/RowItem.kt
@@ -0,0 +1,3 @@
+package entity
+
+data class RowItem(val name: String)
--- app/src/main/java/viewmodel/MainViewModel.kt
+++ app/src/main/java/viewmodel/MainViewModel.kt
@@ -1,7 +1,25 @@
 package viewmodel
 
 import androidx.lifecycle.ViewModel
+import entity.ListData
+import entity.RowItem
+import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
 
-class MainViewModel() :ViewModel(){
+class MainViewModel() : ViewModel() {
+    private val listUpdatePublisher = BehaviorSubject.create<List<RowItem>>()
 
+    fun observeList(): Observable<List<RowItem>> {
+        return listUpdatePublisher
+    }
+
+    fun getList():Observable<ListData>{
+        return listUpdatePublisher;
+    }
+
+    private val rowItemPublisher = BehaviorSubject.create<RowItem>()
+
+    fun observeRowItem(): Observable<RowItem> {
+        return rowItemPublisher
+    }
 }
