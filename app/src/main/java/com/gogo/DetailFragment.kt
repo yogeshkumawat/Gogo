ba1a5a033c31e46d3f9686c34cbac3e5d99606cb
Vipul Sharma <vipul.sharma@timesinternet.in>
2020-09-23 14:16:29 +0530

    layout and viewmodel basic

---
+++ app/src/main/java/com/gogo/DetailFragment.kt
@@ -0,0 +1,31 @@
+package com.gogo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.gogo.databinding.LayoutFragmentDetailBinding
+import viewmodel.MainViewModel
+
+class DetailFragment : Fragment() {
+    lateinit var viewModel: MainViewModel
+    lateinit var binding: LayoutFragmentDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(requireActivity()).get(MainViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.layout_fragment_detail, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/viewmodel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel() :ViewModel(){
+
+}
