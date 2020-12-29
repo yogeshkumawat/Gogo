1f774463570230d3c18c662a59321cca3c3c91f5
Yogesh Kumawat <yogesh.kumawat@timesinternet.in>
2020-12-29 23:06:32 +0530

    Network in progress

---
+++ app/src/main/java/com/gogo/network/NetworkClient.kt
@@ -0,0 +1,15 @@
+package com.gogo.network
+
+import com.gogo.entity.ListData
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+//https://api.github.com/search/users?q=vipul&per_page=3800&page=1
+interface NetworkClient {
+    @GET("/search/users?q={query}&per_page=20&page={page}")
+    fun fetchResult(
+        @Path("query") query: String,
+        @Path("page") page: String
+    ): Call<ListData?>?
+}
---
+++ app/src/main/java/com/gogo/network/NetworkRestClient.kt
@@ -0,0 +1,4 @@
+package com.gogo.network
+
+class NetworkRestClient {
+}
